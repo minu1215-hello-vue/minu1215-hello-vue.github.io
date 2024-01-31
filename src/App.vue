9e2e0f31b517cf2c384436bf4c18312014b4f0ec
minu1215 <mkw3295@gmail.com>
2024-01-31 16:43:46 +0900

    특정 단어 필터링

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,28 @@ import { ref } from 'vue'
 import advice from "@/advice/advice.json"
 
 const advices = ref(advice)
+const filterKeyword = ref('')
+/*
+function filtering(keyword) {
+ console.log(keyword)
+ const authorMatch = advices.author.includes(keyword)
+ const profileMatch = advices.authorProfile.includes(keyword)
+ const messageMatch = advices.message.includes(keyword)
 
+ return authorMatch || profileMatch || messageMatch
+}
+*/
 </script>
 <template>
+ <input v-model="filterKeyword" placeholder="키워드 입력" />
  <div v-for="advice in advices">
-  name : {{ advice.author }} <br>
-  profile : {{ advice.authorProfile }} <br>
-  message : {{ advice.message }} <hr>
+
+  <div v-show="advice.author.includes(filterKeyword) ||
+		advice.authorProfile.includes(filterKeyword) ||
+		advice.message.includes(filterKeyword)">
+   name : {{ advice.author }} <br>
+   profile : {{ advice.authorProfile }} <br>
+   message : {{ advice.message }} <hr>
+  </div>
  </div>
 </template>
