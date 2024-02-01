1db10dd8241522ab9700cf83b3a5f0bc029e6b6c
minu1215 <mkw3295@gmail.com>
2024-02-01 10:13:53 +0900

    App.vue 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
-import { ref } from 'vue'
-import advice from "@/advice/advice.json"
+import { reactive, computed, ref } from 'vue'
 
-const advices = ref(advice)
-const filterKeyword = ref('')
-/*
-function filtering(keyword) {
- console.log(keyword)
- const authorMatch = advices.author.includes(keyword)
- const profileMatch = advices.authorProfile.includes(keyword)
- const messageMatch = advices.message.includes(keyword)
+const author = ref({
+ name: 'John Doe',
+ books: [1, 2, 3]
+})
 
- return authorMatch || profileMatch || messageMatch
-}
-*/
+// a computed ref
+const publishedBookMessage = computed(() => {return author.value.books.length > 2 ? 'Yes' : 'No' })
 </script>
-<template>
- <input v-model="filterKeyword" placeholder="키워드 입력" />
- <div v-for="advice in advices">
 
-  <div v-show="advice.author.includes(filterKeyword) ||
-		advice.authorProfile.includes(filterKeyword) ||
-		advice.message.includes(filterKeyword)">
-   name : {{ advice.author }} <br>
-   profile : {{ advice.authorProfile }} <br>
-   message : {{ advice.message }} <hr>
-  </div>
- </div>
+<template>
+ <p>Has published books:</p>
+ <span>computed ⬇️<br>{{ publishedBookMessage }}</span>
+<!-- <span>{{ author.books.length > 2 ? 'Yes' : 'No' }}</span> -->
 </template>
