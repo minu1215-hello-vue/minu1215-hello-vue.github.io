64d2d6ba9602e33cc44eedaa6fff83ab399b8417
minu1215 <mkw3295@gmail.com>
2024-02-02 10:36:07 +0900

    0.2.12/morning-exercise

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <script setup>
 import { ref } from 'vue'
-import Child from './components/Child.vue'
 
-const pocketMoney = ref('부모 컴포넌트로부터 <💸>을 전달받았어요!')
+const show = ref(true)
+const list = ref([1, 2, 3])
+
+function renderingList() {
+ show.value = !show.value
+}
+
+function addList() {
+ if(list.value.length) {
+  list.value.push(Math.max.apply(null, list.value) + 1)
+ } else {
+  list.value.push(1)
+ }
+}
+
+function deleteList() {
+ if(list.value.length) {
+  list.value.pop()
+ }
+}
+
+function reverseList() {
+ list.value = list.value.reverse()
+}
+
 </script>
 
 <template>
- <Child :pocketMoney="pocketMoney"/>
+ <button @click="renderingList">List 렌더링 ON/OFF </button>
+ <button @click="addList">List 추가 </button>
+ <button @click="deleteList">List 제거  </button>
+ <button @click="reverseList">List 뒤집기 </button>
+
+ <ul v-if="show">
+  <li v-for="item of list">{{ item }}</li>
+ </ul>
+ <p v-else-if="list.length">List is not empty, but hidden.</p>
+ <p v-else>List is empty.</p>
 </template>
